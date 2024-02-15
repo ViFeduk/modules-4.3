55efb32e6deec8fd2fb869bb98e64c5d1926035c
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-15 20:57:32 +0300

    Добавьте файлы проекта.

---
+++ modules 4.3.16/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace modules_4._3._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            var temp = 0;
+            
+            
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < arr.GetLength(1)-1; k++)
+                        {
+                            if (arr[i, k] > arr[i, k + 1])
+                            {
+                                temp = arr[i, k];
+                                arr[i,k]= arr[i, k + 1];
+                                arr[i, k +1]= temp;
+                            }
+                        }
+
+                    }
+                }
+                for (int i = 0;i < arr.GetLength(0); i++)
+                {
+
+                        for (int j = 0;j < arr.GetLength(1); j++)
+                            Console.Write(arr[i,j]+" ");
+                    Console.WriteLine();
+
+                }
+
+
+
+        }
+    }
+}
---
+++ modules 4.3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace modules_4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new int[] { 5, -6, 9, -1, 2, 3, -4 };
+            int temp;
+            int sum=0;
+            int quantity = 0;
+            Console.WriteLine("Сортировка массива");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                 for(int j=0; j<arr.Length-1-i; j++)
+                 {
+
+                 
+                        if(arr[j] > arr[j+1])
+                        {
+                            temp = arr[j];
+                            arr[j] = arr[j +1];
+                            arr[j + 1]=temp;
+                        }
+                 }
+            }
+            for (int i = 0;i < arr.Length;i++)
+                Console.Write(arr[i]);
+            Console.WriteLine();
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine($"Сумма всех чисел равна {sum}");
+
+            for (int i = 0;i< arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    quantity++;
+                }
+            }
+            Console.WriteLine("Кол-во положительных чисел равно "+quantity);
+
+        }
+    }
+}
